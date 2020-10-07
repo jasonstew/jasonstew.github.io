55e148db8124639c48c4ea444cc48564a29583de
jasonstew <jastew1011@gmail.com>
2020-10-07 13:40:38 -0400

    Add files via upload
    added plotly

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 var canvas = document.querySelector('canvas');
 var statusText = document.querySelector('#statusText');
 var sensorselect = document.getElementById("sensorselect");
-
 statusText.addEventListener('click', function() {
   statusText.textContent = 'connecting...';
   heartRates = [];
   options =[];
+
   heartRateSensor.connect()
   .then(() => addList());
+  //.then(() => plotData());
+
   //.then(() => heartRateSensor.startNotificationsHeartRateMeasurement().then(handleHeartRateMeasurement))
 //  .catch(error => {
   //  statusText.textContent = error;
@@ -17,7 +19,7 @@ statusText.addEventListener('click', function() {
 
 
 });
-sensorselect.addEventListener('change', function() {
+sensorselect.addEventListener('click', function() {
 heartRates = [];
   options =[];
 var j;
@@ -25,82 +27,114 @@ var j;
  {
    heartRateSensor.stopNotificationsHeartRateMeasurement(j);
     }
-    
- heartRateSensor.startNotificationsHeartRateMeasurement(sensorselect.selectedIndex).then(handleHeartRateMeasurement);
+        var time = new Date();
+    var data = [{
+      x: [time],
+      y: [0],
+      mode: 'lines',
+      line: {color: '#80CAF6'}
+    }]
+
+    var dataCard = [
+      {
+        domain: { x: [0, 1], y: [0, 1] },
+        value: 0,
+        title: { text: "Humidity" },
+        type: "indicator",
+        mode: "gauge+number",
+        delta: { reference: 0 },
+        gauge: { axis: { range: [null, 500] } }
+      }
+    ];
+
+    var layout = { width: 600, height: 400 };
+    Plotly.newPlot('dataCard', dataCard, layout);
+
+    Plotly.newPlot('myDiv', data);
+ heartRateSensor.startNotificationsHeartRateMeasurement(1).then(handleHeartRateMeasurement);
 //  .catch(error => {
   //  statusText.textContent = error;
   //});
 
 });
+function plotData(heartRateMeasurement){
+
+
+
+
+  var data = [{
+    x: [time],
+    y: [heartRateMeasurement],
+    mode: 'lines',
+    line: {color: '#80CAF6'}
+  }]
+
+  Plotly.newPlot('myDiv', data);
+
+  var cnt = 0;
+
+  var interval = setInterval(function() {
+
+    var time = new Date();
+
+
+    Plotly.relayout('myDiv', minuteView);
+    Plotly.extendTraces('myDiv', update, [0])
+
+    if(++cnt === 100) clearInterval(interval);
+  }, 1000);
+}
 function addList(){
   var i;
-  sensorselect.style.visibility = "visibile";
+  $('#connected_success').show();
+  $('#dropdown').show();
+    $('#button_container').hide();
+
   for( i=0; i< heartRateSensor.options.length; i++)
  {
    var option = document.createElement("option");
-   option.text=heartRateSensor.options[i].uuid;
-   sensorselect.add(option,sensorselect[i+1]);
+   //option.text=heartRateSensor.options[i].uuid;
+//   sensorselect.append('<a class="dropdown-item" href="#"> "$heartRateSensor.options[i].uuid" </a>');
+
+    sensorselect.innerHTML += `<a class="dropdown-item" href="#">${heartRateSensor.options[i].uuid}</a>`;
+
  }
  statusText.textContent = 'Select Sensor...';
 }
 function handleHeartRateMeasurement(heartRateMeasurement) {
   heartRateMeasurement.addEventListener('characteristicvaluechanged', event => {
     var heartRateMeasurement = heartRateSensor.parseHeartRate(event.target.value);
-    statusText.innerHTML = heartRateMeasurement;
-    heartRates.push(heartRateMeasurement);
-    drawWaves();
+    var time = new Date();
+    var update = {
+    x:  [[time]],
+    y: [[heartRateMeasurement]]
+    }
+
+    var olderTime = time.setMinutes(time.getMinutes() - 1);
+    var futureTime = time.setMinutes(time.getMinutes() + 1);
+
+    var minuteView = {
+          xaxis: {
+            type: 'date',
+            range: [olderTime,futureTime]
+          }
+        };
+        var dataCardUpdate = {
+            value: heartRateMeasurement
+        };
+    Plotly.restyle('dataCard', dataCardUpdate);
+    Plotly.relayout('myDiv', minuteView);
+    Plotly.extendTraces('myDiv', update, [0])
+//    statusText.innerHTML = heartRateMeasurement;
+  //  heartRates.push(heartRateMeasurement);
+  //  drawWaves();
   });
 }
 
 var heartRates = [];
 var mode = 'line';
 
-canvas.addEventListener('click', event => {
-  mode = mode === 'bar' ? 'line' : 'bar';
-  drawWaves();
-});
-
-function drawWaves() {
-  requestAnimationFrame(() => {
-    canvas.width = parseInt(getComputedStyle(canvas).width.slice(0, -2)) * devicePixelRatio;
-    canvas.height = parseInt(getComputedStyle(canvas).height.slice(0, -2)) * devicePixelRatio;
-
-    var context = canvas.getContext('2d');
-    var margin = 2;
-    var max = Math.max(0, Math.round(canvas.width / 11));
-    var offset = Math.max(0, heartRates.length - max);
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.strokeStyle = '#00796B';
-    if (mode === 'bar') {
-      for (var i = 0; i < Math.max(heartRates.length, max); i++) {
-        var barHeight = Math.round(heartRates[i + offset ] * canvas.height / 200);
-        context.rect(11 * i + margin, canvas.height - barHeight, margin, Math.max(0, barHeight - margin));
-        context.stroke();
-      }
-    } else if (mode === 'line') {
-      context.beginPath();
-      context.lineWidth = 6;
-      context.lineJoin = 'round';
-      context.shadowBlur = '1';
-      context.shadowColor = '#333';
-      context.shadowOffsetY = '1';
-      for (var i = 0; i < Math.max(heartRates.length, max); i++) {
-        var lineHeight = Math.round(heartRates[i + offset ] * canvas.height / 200);
-        if (i === 0) {
-          context.moveTo(11 * i, canvas.height - lineHeight);
-        } else {
-          context.lineTo(11 * i, canvas.height - lineHeight);
-        }
-        context.stroke();
-      }
-    }
-  });
-}
-
-window.onresize = drawWaves;
-
-document.addEventListener("visibilitychange", () => {
-  if (!document.hidden) {
-    drawWaves();
-  }
-});
+//canvas.addEventListener('click', event => {
+//  mode = mode === 'bar' ? 'line' : 'bar';
+//  drawWaves();
+//});
