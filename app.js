cedaa414c1c06beac305aa8bb9879710e5801ede
jasonstew <jastew1011@gmail.com>
2020-10-12 12:13:36 -0400

    buffer
    fixed buffer and added plotly graphs

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 var canvas = document.querySelector('canvas');
 var statusText = document.querySelector('#statusText');
 var sensorselect = document.getElementById("sensorselect");
@@ -138,3 +139,266 @@ var mode = 'line';
 //  mode = mode === 'bar' ? 'line' : 'bar';
 //  drawWaves();
 //});
+=======
+var canvas = document.querySelector('canvas');
+var statusText = document.querySelector('#statusText');
+var myTab = document.getElementById("myTab");
+var dataContainer = document.getElementById("dataContainer");
+var variableNames = ["Pressure 1", "Pressure 2", "Pressure 3", "Pressure 4", "Pressure 5", "PID 1", "PID 2", "PID 3", "Flow 1", "Flow 2", "Temperature", "Humidity"]
+statusText.addEventListener('click', function() {
+  statusText.textContent = 'connecting...';
+  heartRates = [];
+  options = [];
+
+  heartRateSensor.connect()
+    .then(() => addList());
+  //.then(() =>  heartRateSensor.startNotificationsHeartRateMeasurement());
+  //  .then(() => handleHeartRateMeasurement);
+
+  //.then(() => heartRateSensor.startNotificationsHeartRateMeasurement().then(handleHeartRateMeasurement))
+  //  .catch(error => {
+  //  statusText.textContent = error;
+  //});
+  //  dropdown.style.visibility = "visibile";
+
+
+
+});
+
+async function initializePlots(heartRateMeasurement) {
+
+  var time = new Date();
+  var i;
+  for (i = 0; i < heartRateMeasurement.SensorVal.length; i++) {
+    if (typeof heartRateMeasurement.SensorVal[i] !== 'undefined') {
+
+      var cardString = variableNames[i] + "dataCard";
+      var divString = variableNames[i] + "myDiv";
+
+      var cardElement = document.getElementById(cardString);
+      var divElement = document.getElementById(divString);
+      var olderTime = time.setMinutes(time.getMinutes() - 1);
+      var futureTime = time.setMinutes(time.getMinutes() + 1);
+
+
+      if (cardElement == null) {
+
+        var minuteView = {
+          xaxis: {
+            type: 'date',
+            range: [olderTime, futureTime]
+          }
+        };
+        var data = {
+          x: [time],
+          y: [heartRateMeasurement.SensorVal[i]],
+          mode: 'lines',
+          line: {
+            color: '#80CAF6'
+          }
+        };
+        var dataCard = [{
+          domain: {
+            x: [0, 1],
+            y: [0, 1]
+          },
+          value: 450,
+          title: {
+            text:  variableNames[i]
+          },
+          type: "indicator",
+          mode: "gauge+number",
+          delta: {
+            reference: 400
+          },
+          gauge: {
+            axis: {
+              range: [null, 100]
+            }
+          }
+        }];
+        var layout = {
+          width: 600,
+          height: 400
+        };
+        var config = {
+          responsive: true
+        }
+        var para1 = document.createElement("div");
+        para1.id = cardString;
+        dataContainer.appendChild(para1);
+        var para2 = document.createElement("div");
+        para2.id = divString;
+        dataContainer.appendChild(para2);
+        //  dataContainer.innerHTML += `<div id="${cardString}"></div>
+        //<div id="${divString}"></div>`
+        Plotly.plot(cardString, dataCard, config)
+          .then(Plotly.plot(divString, [data],  config).then(function() {
+            window.requestAnimationFrame(function() {
+              window.requestAnimationFrame(function() {
+            //    window.alert('Your plot is done.');
+              });
+            });
+          }));
+      }
+    }
+  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(heartRateMeasurement);
+    }, 3 * 1000);
+  });
+}
+// sensorselect.addEventListener('click', function() {
+// heartRates = [];
+//   options =[];
+// var j;
+//  for ( j=1; j< heartRateSensor.options.length; j++)
+//  {
+//    heartRateSensor.stopNotificationsHeartRateMeasurement(j);
+//     }
+//         var time = new Date();
+//     var data = [{
+//       x: [time],
+//       y: [0],
+//       mode: 'lines',
+//       line: {color: '#80CAF6'}
+//     }]
+//
+//     var dataCard = [
+//       {
+//         domain: { x: [0, 1], y: [0, 1] },
+//         value: 0,
+//         title: { text: "Humidity" },
+//         type: "indicator",
+//         mode: "number",
+//       }
+//     ];
+//
+//     var layout = { width: 600, height: 400 };
+//     var config = {responsive: true}
+//     Plotly.newPlot('dataCard', dataCard, layout,config);
+//
+//     Plotly.newPlot('myDiv', data,config);
+//  heartRateSensor.startNotificationsHeartRateMeasurement(1).then(handleHeartRateMeasurement);
+// //  .catch(error => {
+//   //  statusText.textContent = error;
+//   //});
+//
+// });
+// function plotData(heartRateMeasurement){
+//
+//
+//
+//   var data = [{
+//     x: [time],
+//     y: [heartRateMeasurement],
+//     mode: 'lines',
+//     line: {color: '#80CAF6'}
+//   }]
+//
+//   Plotly.newPlot('myDiv', data);
+//
+//   var cnt = 0;
+//
+//   var interval = setInterval(function() {
+//
+//     var time = new Date();
+//
+//
+//     Plotly.relayout('myDiv', minuteView);
+//     Plotly.extendTraces('myDiv', update, [0])
+//
+//     if(++cnt === 100) clearInterval(interval);
+//   }, 1000);
+// }
+function addList() {
+  var i;
+
+  $('#button_container').hide();
+
+
+  //var option = document.createElement("option");
+  //option.text=heartRateSensor.options[i].uuid;
+  //   sensorselect.append('<a class="dropdown-item" href="#"> "$heartRateSensor.options[i].uuid" </a>');
+
+  //  sensorselect.innerHTML += `<a class="dropdown-item" href="#">${heartRateSensor.options[i].uuid}</a>`;
+
+
+  // statusText.textContent = 'Select Sensor...';
+  heartRateSensor.startNotificationsHeartRateMeasurement().then(handleHeartRateMeasurement);
+
+  //handleHeartRateMeasurement();
+}
+
+function handleHeartRateMeasurement(heartRateMeasurement) {
+  const once = {
+    once: true
+  };
+  heartRateMeasurement.addEventListener('characteristicvaluechanged', event => {
+    var heartRateMeasurement = heartRateSensor.parseHeartRate(event.target.value);
+    initializePlots(heartRateMeasurement)
+      .then(heartRateMeasurement => plotData(heartRateMeasurement));
+  });
+  //const promise2= promise.then(()=>plotData));
+  //heartRateMeasurement.addEventListener('characteristicvaluechanged', event =>plotData);
+
+
+}
+
+function plotData(heartRateMeasurement) {
+
+  //  var heartRateMeasurement = heartRateSensor.parseHeartRate(event.target.value);
+  var time = new Date();
+  var i;
+  for (i = 0; i < heartRateMeasurement.SensorVal.length; i++) {
+    if (typeof heartRateMeasurement.SensorVal[i] !== 'undefined') {
+
+      var cardString = variableNames[i] + "dataCard";
+      var divString = variableNames[i] + "myDiv";
+
+      var cardElement = document.getElementById(cardString);
+      var divElement = document.getElementById(divString);
+      var olderTime = time.setMinutes(time.getMinutes() - 1);
+      var futureTime = time.setMinutes(time.getMinutes() + 1);
+
+      var minuteView = {
+        xaxis: {
+          type: 'date',
+          range: [olderTime, futureTime]
+        }
+      };
+
+
+      if (cardElement !== null) {
+        var data = {
+          x: [
+            [time]
+          ],
+          y: [
+            [heartRateMeasurement.SensorVal[i]]
+          ]
+        }
+        var dataCard = {
+          value: heartRateMeasurement.SensorVal[i]
+        };
+
+        Plotly.relayout(divString, minuteView);
+        Plotly.extendTraces(divString, data, [0]);
+        Plotly.restyle(cardString, dataCard);
+
+      }
+
+      //    statusText.innerHTML = heartRateMeasurement;
+      //  heartRates.push(heartRateMeasurement);
+      //  drawWaves();
+    }
+  }
+
+}
+
+//canvas.addEventListener('click', event => {
+//  mode = mode === 'bar' ? 'line' : 'bar';
+//  drawWaves();
+//});
+>>>>>>> Stashed changes
--- heartRateSensor.js
+++ heartRateSensor.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 (function() {
   'use strict';
 
@@ -135,3 +136,186 @@
   window.heartRateSensor = new HeartRateSensor();
 
 })();
+=======
+(function() {
+  'use strict';
+
+  class HeartRateSensor {
+    constructor() {
+      this.device = null;
+      this.server = null;
+      this._characteristics = new Map();
+      this.options =[];
+    }
+    connect() {
+      return navigator.bluetooth.requestDevice({filters:[{services:[ '4677062c-ad02-4034-9abf-98581772427c' ]}]})
+      .then(device => {
+        this.device = device;
+        return device.gatt.connect();
+      })
+      .then(server => {
+        this.server = server;
+        return Promise.all([
+          server.getPrimaryService('4677062c-ad02-4034-9abf-98581772427c').then(service => {
+      //      return Promise.all([
+      //        this._cacheCharacteristic(service, 'dc13b36a-3499-46b0-ac11-5ac0173c4cc5'),
+      //        this._cacheCharacteristic(service, 'b4250402-fb4b-4746-b2b0-93f0e61122c6'),
+      //      ])
+            return service.getCharacteristics()
+            .then(characteristics => {
+
+
+            this.options=  characteristics;
+            })
+          //  .then( () =>statusText.textContent = 'Select Sensor');
+          })
+        ]);
+      })
+    }
+
+    /* Heart Rate Service */
+
+    // getBodySensorLocation() {
+    //   return this._readCharacteristicValue('dc13b36a-3499-46b0-ac11-5ac0173c4cc5')
+    //   .then(data => {
+    //     let sensorLocation = data.getUint8(0);
+    //     switch (sensorLocation) {
+    //       case 0: return 'Other';
+    //       case 1: return 'Chest';
+    //       case 2: return 'Wrist';
+    //       case 3: return 'Finger';
+    //       case 4: return 'Hand';
+    //       case 5: return 'Ear Lobe';
+    //       case 6: return 'Foot';
+    //       default: return 'Unknown';
+    //     }
+    //  });
+    // }
+    startNotificationsHeartRateMeasurement() {
+
+    //  return this._startNotifications(heartRateSensor.options[index].uuid);
+
+        return this.options[0].startNotifications();
+    }
+    stopNotificationsHeartRateMeasurement(index) {
+    //  return this._stopNotifications(heartRateSensor.options[index].uuid);
+        return this.options[0].stopNotifications();
+    }
+    parseHeartRate(value) {
+      // In Chrome 50+, a DataView is returned instead of an ArrayBuffer.
+      value = value.buffer ? value : new DataView(value);
+
+
+      let result = {};
+
+
+      result.config = value.getUint16(0,true);
+      result.config= result.config.toString(2);
+      result.SensorVal=[];
+      // if (rate16Bits) {
+      var  i;
+      for (i=1; i<= result.config.length; i++)
+      {
+
+      result.SensorVal[12-i] = value.getUint16(2*i,true)/100;
+
+      }
+
+      //   index += 2;
+      // } else {
+      //   result.heartRate = value.getUint8(index);
+      //   index += 1;
+      // }
+      // let contactDetected = flags & 0x2;
+      // let contactSensorPresent = flags & 0x4;
+      // if (contactSensorPresent) {
+      //   result.contactDetected = !!contactDetected;
+      // }
+      // let energyPresent = flags & 0x8;
+      // if (energyPresent) {
+      //   result.energyExpended = value.getUint16(index, /*littleEndian=*/true);
+      //   index += 2;
+      // }
+      // let rrIntervalPresent = flags & 0x10;
+      // if (rrIntervalPresent) {
+      //   let rrIntervals = [];
+      //   for (; index + 1 < value.byteLength; index += 2) {
+      //     rrIntervals.push(value.getUint16(index, /*littleEndian=*/true));
+      //   }
+      //   result.rrIntervals = rrIntervals;
+      // }
+      return result;
+    }
+
+    /* Utils */
+
+    _cacheCharacteristic(service, characteristicUuid) {
+      return service.getCharacteristic(characteristicUuid)
+      .then(characteristic => {
+        this._characteristics.set(characteristicUuid, characteristic);
+      });
+    }
+    _readCharacteristicValue(characteristicUuid) {
+      let characteristic = this._characteristics.get(characteristicUuid);
+      return characteristic.readValue()
+      .then(value => {
+        // In Chrome 50+, a DataView is returned instead of an ArrayBuffer.
+        value = value.buffer ? value : new DataView(value);
+        return value;
+      });
+    }
+    _writeCharacteristicValue(characteristicUuid, value) {
+      let characteristic = this._characteristics.get(characteristicUuid);
+      return characteristic.writeValue(value);
+    }
+    _startNotifications(characteristicUuid) {
+      let characteristic = this._characteristics.get(characteristicUuid);
+      // Returns characteristic to set up characteristicvaluechanged event
+      // handlers in the resolved promise.
+      return characteristic.startNotifications()
+      .then(() => characteristic);
+    }
+    _stopNotifications(characteristicUuid) {
+      let characteristic = this._characteristics.get(characteristicUuid);
+      // Returns characteristic to remove characteristicvaluechanged event
+      // handlers in the resolved promise.
+      return characteristic.stopNotifications()
+      .then(() => characteristic);
+    }
+    getFloatValue(value) {
+      var offset=0;
+    // if the last byte is a negative value (MSB is 1), the final
+    // float should be too
+    const negative = value.getInt8(offset + 2) >>> 31;
+
+    // this is how the bytes are arranged in the byte array/DataView
+    // buffer
+    const [b0, b1, b2, exponent] = [
+        // get first three bytes as unsigned since we only care
+        // about the last 8 bits of 32-bit js number returned by
+        // getUint8().
+        // Should be the same as: getInt8(offset) & -1 >>> 24
+        value.getUint8(offset),
+        value.getUint8(offset + 1),
+        value.getUint8(offset + 2),
+
+        // get the last byte, which is the exponent, as a signed int
+        // since it's already correct
+        value.getInt8(offset + 3)
+    ];
+
+    let mantissa = b0 | (b1 << 8) | (b2 << 16);
+    if (negative) {
+        // need to set the most significant 8 bits to 1's since a js
+        // number is 32 bits but our mantissa is only 24.
+        mantissa |= 255 << 24;
+    }
+
+    return mantissa * Math.pow(10, exponent);
+}
+  }
+
+  window.heartRateSensor = new HeartRateSensor();
+
+})();
+>>>>>>> Stashed changes
